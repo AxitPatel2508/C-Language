89b646a15040b3004ac4eed927b86d1aedc48342
Axit Patel <patelaxit22844@gmail.com>
2025-07-14 16:27:06 +0530

    Add files via upload

---
+++ Array/p1.c
@@ -0,0 +1,14 @@
+// wap to print all elements of array.
+
+#include <stdio.h>
+
+void main()
+{
+    int i;
+    int a[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+
+    for (i = 0; i <= 9; i++)
+    {
+        printf("%d ", a[i]);
+    }
+}
