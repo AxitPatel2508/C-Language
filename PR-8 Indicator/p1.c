c8ec9481342ac840f78b9ddfdeb71dc661389076
Axit Patel <patelaxit22844@gmail.com>
2025-08-14 11:46:06 +0530

    Add files via upload

---
+++ PR-8 Indicator/p1.c
@@ -0,0 +1,24 @@
+// Q.1 Length Finder
+// Develop a program that finds the length of a string using a pointer.
+
+#include <stdio.h>
+
+void main()
+{
+    char name[10];
+    char *ptr;
+    int len = 0;
+
+    printf("Enter any String:");
+    scanf("%s", name);
+
+    ptr = name;
+
+    while (*ptr != '\0')
+    {
+        len++;
+        ptr++;
+    }
+
+    printf("Length of String :%d\n", len);
+}
